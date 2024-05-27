a7c3bb93a4d323fe21c705cd8216a0284c78afdb
Monu_2114 <monu.nanabala@gmail.com>
2024-05-27 19:59:55 +0530

    Timer

--- src/app/page.js
+++ src/app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
     }
     setSelectedMovies(newMovies);
   }
-
+  
   return ( 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <Image className = "opacity-40" src="/Untitled design.png" alt="My Image"  layout="fill" objectFit="cover"/>
@@ -48,6 +48,9 @@ export default function Home() {
           <Card image={movie.image} movieName={movie.movieName} year={movie.year} actors={movie.actors} director={movie.director}/>
         ))}
       </div>
+      <div className="bg-gray-700 max-h-10.5 min-w-5 rounded-xl	">
+      <button className="  px-10 py-10 text-white text-2xl font-bold bg-gray-700 hover:bg-gray-500">5:00</button>
+</div>
       <div className="flex items-start justify-center z-10 ">
       <button type="button" className="hover:bg-blue-800  text-white bg-blue-700 text-lg px-6 py-4 mt-[-35vh] rounded-md" onClick={handleClick}>Inka !!</button>
      
