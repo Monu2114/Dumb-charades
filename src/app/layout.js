cedac1e10303d685a9d2c439cc413d9f231f558f
Monu_2114 <monu.nanabala@gmail.com>
2024-09-23 21:13:27 +0530

    favicon and title

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+
+      <title>Dumb Charades</title>
       <body className={inter.className}>{children}</body>
     </html>
   );
